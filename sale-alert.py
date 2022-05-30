b7832199031ac803a39ff40a0851c5dad359e17e
Filip Drygaś <tejotemku@gmail.com>
2022-05-30 14:42:10 +0200

    added ggdeals

--- sale-alert.py
+++ sale-alert.py
@@ -1,6 +1,6 @@
 import telegram_send
 import requests
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from time import mktime, gmtime, time, sleep
 import json
 import sys
@@ -30,8 +30,18 @@ xkom_link = "https://www.x-kom.pl/goracy_strzal/"
 # ---------------------------------
 #   Epic games store config
 # ---------------------------------
-epic_free_game_link = "https://www.epicgames.com/store/en-US/free-games"
+epic_games_link_free_game = "https://www.epicgames.com/store/en-US/free-games"
 epic_games_free_games_api_link = "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=PL&allowCountries=PL"
+
+
+# ---------------------------------
+#   GG.Deals config
+# ---------------------------------
+ggdeals_weekdays = [1, 2, 3, 4, 5, 6, 7]
+ggdeals_hours = [(18, 1)]
+ggdeals_link_cluster = "https://gg.deals/news/?availability=1&title=Free+game&type=1,6"
+ggdeals_link_only_humble_bundle = "https://gg.deals/eu/news/humble-bundle-free-games/"
+ggdeals_link_news_younger_than = timedelta(days=1)
 
 def send_info_sale(site:str, name:str, old_price:float, new_price:float, link:str=None) -> None:
     msg = f"{site}\n{name}\nOld Price: {old_price}\nNew Price: {new_price}"
@@ -45,7 +55,7 @@ def send_info_sale(site:str, name:str, old_price:float, new_price:float, link:st
 
 
 def send_info_free_game(shop:str, name:str, link:str=None, end_date:datetime=None) -> None:
-    msg = f"FREE GAME on - {shop}\nTitle - {name}"
+    msg = f"FREE GAME - {shop}\nTitle - {name}"
     if end_date:
         msg += f"\nDeadline - {end_date}"
     if link:
@@ -71,8 +81,7 @@ def get_html(site_url):
     web_page = requests.get(site_url, headers=header).text
     return BeautifulSoup(web_page, "html.parser")
 
-
-def check_site_by_weekday(weekday, hour, site_weekdays, site_hours):
+def check_site_by_weekday_and_hour(weekday, hour, site_weekdays, site_hours):
     return hour in site_hours and weekday in site_weekdays
 
 
@@ -81,6 +90,7 @@ def check_site_by_next_date(next_date, now):
 
 
 def morele():
+    print("Searching morele...")
     try:
         soup = get_html(morele_link)
         name = soup.find("div", {"class": "promo-box-name"})
@@ -99,6 +109,7 @@ def morele():
 
 
 def xkom():
+    print("Searching xkom...")
     try:
         soup = get_html(xkom_link)
         name = soup.find("title", {}).text
@@ -125,7 +136,44 @@ def xkom():
         print(E)
 
 
-def epic_games_store(send_msg=True):
+def ggdeals():
+    ggdeals_get_posts(ggdeals_link_only_humble_bundle, "Searching ggdeals for humble bundle...", "Humble Bundle")
+    ggdeals_get_posts(ggdeals_link_cluster, "Searching ggdeals for freebies...", "GG.Deals")
+    
+
+def ggdeals_get_posts(url, msg, shop):
+    print(msg)
+    try:
+        soup = get_html(url)
+        offer_list = soup.find("div", {"class":"list-items news-list"})
+        offers = offer_list.find_all("div", {"class":"hoverable-box"})
+        utc_time = datetime.utcnow()
+
+        for offer in offers:
+            time_added = str(offer.find("time"))
+            date_index_start = time_added.find("datetime=\"") + len("datetime=\"")
+            date_index_end = time_added.find("\"", date_index_start + 2 )
+            post_date = time_added[date_index_start:date_index_end]
+            post_date = post_date[:post_date.find('+')]
+            if utc_time - datetime.strptime(post_date, "%Y-%m-%dT%H:%M:%S") < ggdeals_link_news_younger_than:
+                game_name = offer.find("h3", {"class":"news-title"}).text
+                game_name = game_name.replace("FREE ", "")
+                post_link = str(offer.find("a", {"class": "full-link"}))
+                post_link_index_start = post_link.find("href=\"") + len("href=\"")
+                post_link_index_end = post_link.find("\"", post_link_index_start + 2 )
+                link = "gg.deals" + post_link[post_link_index_start:post_link_index_end]
+                print(game_name + " - " + link)
+                send_info_free_game(
+                    shop=shop,
+                    name=game_name,
+                    link=link
+                )
+
+    except Exception as E:
+        print(E)
+
+def epic_games_store():
+    print("Searching epic games store for freebies...")
     try:
         egs_data = requests.get(epic_games_free_games_api_link).text
         data = json.loads(egs_data)
@@ -146,12 +194,11 @@ def epic_games_store(send_msg=True):
                                                                           '20%y-%m-%dT%H:%M:%S.%fZ') + time_zone_hour_diff
                                     
                                     if start_date < today < end_date:
-                                        if send_msg:
-                                            send_info_free_game(
-                                                shop="Epic Games Store", 
-                                                name=title["title"], 
-                                                link=epic_free_game_link,
-                                                end_date=end_date)
+                                        send_info_free_game(
+                                            shop="Epic Games Store", 
+                                            name=title["title"], 
+                                            link=epic_games_link_free_game,
+                                            end_date=end_date)
                                         sites[epic_games_store].update({"next_date": end_date - timedelta(minutes=1, seconds=1)})
                                         print(">>>found epic games store game ", title["title"])
     except Exception as E:
@@ -167,7 +214,7 @@ def clock():
         minute = now.minute
         for key, value in sites.items():
             if value["schedule"] == "fixed_schedule":
-                if check_site_by_weekday(weekday, (hour, minute), value["weekdays"], value["hours"]):
+                if check_site_by_weekday_and_hour(weekday, (hour, minute), value["weekdays"], value["hours"]):
                     key()
             elif value["schedule"] == "fluid_period_schedule":
                 if check_site_by_next_date(value["next_date"], now):
@@ -175,18 +222,21 @@ def clock():
 
 
 def start():
-    epic_games_store(send_msg=True)
+    epic_games_store()
     xkom()
     morele()
+    ggdeals()
     clock()
 
 
 sites = {
     morele: {"schedule": "fixed_schedule", "weekdays": morele_weekdays, "hours": morele_hours},
     xkom: {"schedule": "fixed_schedule", "weekdays": xkom_weekdays, "hours": xkom_hours},
-    epic_games_store: {"schedule": "fluid_period_schedule", "next_date": None}
+    epic_games_store: {"schedule": "fluid_period_schedule", "next_date": None},
+    ggdeals: {"schedule": "fixed_schedule","weekdays": ggdeals_weekdays, "hours": ggdeals_hours }
 }
 
 # telegram_send.configure(conf="onion-master-config.conf", group=True)
 DEBUG = "-d" in sys.argv[1:]
-start()
+# start()
+ggdeals()
